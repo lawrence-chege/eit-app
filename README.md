26a6fd8b1b43dd6fd7625cf67fed3c4acbec7f2f
lawrence-chege <lawrence.chege@meltwater.org>
2019-10-20 18:27:25 +0000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# tic-tac
-A react starter game
---
+++ precache-manifest.2e3f940a33c01308c16484434645a272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5067e9804e8fb960dd23828acaba20cf",
+    "url": "/tic-tac/index.html"
+  },
+  {
+    "revision": "e4455a9cde43246d936d",
+    "url": "/tic-tac/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "472bc42b1807341db96f",
+    "url": "/tic-tac/static/js/2.2ee4132b.chunk.js"
+  },
+  {
+    "revision": "e4455a9cde43246d936d",
+    "url": "/tic-tac/static/js/main.4faf9a37.chunk.js"
+  },
+  {
+    "revision": "16df7ad588724f6676f4",
+    "url": "/tic-tac/static/js/runtime-main.f1ef0465.js"
+  }
+]);
