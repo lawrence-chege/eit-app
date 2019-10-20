7a8799e13fe0138e80923f633cde90667c0017a0
lawrence-chege <lawrence.chege@meltwater.org>
2019-10-20 14:24:10 +0000

    display items using props

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
+import Table from './Table';
 import './App.css';
 
 class App extends Component {
 render(){
+  const characters = [
+    {
+      name: 'Charlie',
+      job: 'Janitor',
+    },
+    {
+      name: 'Mac',
+      job: 'Bouncer',
+    },
+    {
+      name: 'Dee',
+      job: 'Aspring actress',
+    },
+    {
+      name: 'Dennis',
+      job: 'Bartender',
+    },
+  ]
   return(
-    <div className="App">
+    <div className="container">
       <h1>Hello World</h1>
+      <Table charactersData = {characters}/>
     </div>
   )
 }
